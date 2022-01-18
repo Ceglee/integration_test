b3b5f1aabdc1cbdf030fb4f399d7302574814cab
CegLee <ceglarzp@gmail.com>
2022-01-18 16:14:54 +0100

    Logging time execution

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+import time
+
 from flask import Flask, Response, request, json
 
 from user_details_service import UserDetailsService
@@ -9,8 +11,10 @@ USER_DETAILS_SERVICE = UserDetailsService()
 
 @app.route("/", methods=["POST"])
 def get_user_details():
+    start = time.time_ns()
     if validate(request.json):
         user_details = USER_DETAILS_SERVICE.get_user_details(request.json["user_name"])
+        print("Message processed in: " + str(time.time_ns() - start) + " nanoseconds.")
         return user_details or Response(response=json.dumps({"message": "Unable to retrieve user details."}), status=204, mimetype="application/json")
 
     else:
