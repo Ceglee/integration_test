7e331b7a80676269f1473d7f293daf45c7b4a2b1
CegLee <ceglarzp@gmail.com>
2022-01-19 10:47:37 +0100

    One more test

--- src/app_test.py
+++ src/app_test.py
@@ -26,6 +26,12 @@ class MyTestCase(unittest.TestCase):
             result = app.get_user_details()
             self.assertEqual(result.status_code, 422)
 
+    def test_On_get_user_details_Should_return_422_When_user_name_is_of_invalid_type(self):
+        m = mock.MagicMock()
+        m.json = {"user_name": 123}
+        with mock.patch("app.request", m):
+            result = app.get_user_details()
+            self.assertEqual(result.status_code, 422)
 
 if __name__ == '__main__':
     unittest.main()
